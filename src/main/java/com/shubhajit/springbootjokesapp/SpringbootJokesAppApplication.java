c52ebf0fc24a2953123889294e5910605be5a276
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-05-25 04:27:52 +0530

    spring xml configuration demo

--- src/main/java/com/shubhajit/springbootjokesapp/SpringbootJokesAppApplication.java
+++ src/main/java/com/shubhajit/springbootjokesapp/SpringbootJokesAppApplication.java
@@ -2,8 +2,10 @@ package com.shubhajit.springbootjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chucknorris-config.xml")
 public class SpringbootJokesAppApplication {
 
 	public static void main(String[] args) {
