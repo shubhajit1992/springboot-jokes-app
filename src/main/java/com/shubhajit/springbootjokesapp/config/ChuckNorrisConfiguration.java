c4c02d467e5ab03702f4e9edd69dcaaf38b8f670
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-05-25 04:11:32 +0530

    adding java configuration

---
+++ src/main/java/com/shubhajit/springbootjokesapp/config/ChuckNorrisConfiguration.java
@@ -0,0 +1,14 @@
+package com.shubhajit.springbootjokesapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckNorrisConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
