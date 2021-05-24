adb4007721b79205425f65f92b86f03ccf13f7f8
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-05-25 04:04:56 +0530

    spring boot Jokes App

---
+++ src/main/java/com/shubhajit/springbootjokesapp/SpringbootJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.shubhajit.springbootjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJokesAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/shubhajit/springbootjokesapp/controller/JokeController.java
@@ -0,0 +1,22 @@
+package com.shubhajit.springbootjokesapp.controller;
+
+import com.shubhajit.springbootjokesapp.service.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @GetMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
